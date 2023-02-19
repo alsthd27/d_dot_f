bdc3e04e2387827d9f279c23957952fa0736947b
Yomin Song <alsthd27@naver.com>
2023-02-19 20:46:10 +0900

    edit settings.py for staticfiles

--- d_dot_f/settings.py
+++ d_dot_f/settings.py
@@ -154,9 +154,11 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 STATICFILES_DIRS = [BASE_DIR / "d_dot_f" / "static"]
+
+STATIC_ROOT = (BASE_DIR / "d_dot_f" / "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
