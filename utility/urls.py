a53fbd04d47aa361e3adf91adc71a7ed3c53e96f
Yomin Song <alsthd27@naver.com>
2023-02-26 16:47:30 +0900

    test

--- utility/urls.py
+++ utility/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from .d_utils import *
-from .views import test
 
 app_name = "utility"
 urlpatterns = [
@@ -8,5 +7,5 @@ urlpatterns = [
     path("confirm-vcode", confirm_vcode, name="confirm_vcode"),
     path("update-dmd-cookie", update_dmd_cookie, name="update_dmd_cookie"),
     path("delete-expired-vcodes", delete_expired_vcodes, name="delete_expired_vcodes"),
-    path("delete-inactive-users", delete_inactive_users, name="delete_inactive_users")
+    path("delete-inactive-users", delete_inactive_users, name="delete_inactive_users"),
 ]
