c8a969d066d1f61569b417ae18dc79a259a7fd40
Yomin Song <alsthd27@naver.com>
2023-02-26 16:14:02 +0900

    test for 'update_dmd_cookie' function

--- utility/urls.py
+++ utility/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .d_utils import *
+import views
 
 app_name = "utility"
 urlpatterns = [
+    path("test", views.update_dmd_cookie),
     path("create-vcode", create_vcode, name="create_vcode"),
     path("confirm-vcode", confirm_vcode, name="confirm_vcode"),
     path("update-dmd-cookie", update_dmd_cookie, name="update_dmd_cookie"),
